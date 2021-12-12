df85bf7a6f629b6fd2dd067b649c82c397318e70
Behzad <saeedibehzad@gmail.com>
2021-12-12 16:45:34 +0330

    add navbar section

---
+++ script.js
@@ -0,0 +1,7 @@
+const showMenu=()=>{
+    document.querySelector(".nav__lists").classList.toggle("show__menu")  
+    document.querySelector("#col_1").classList.toggle("times1")
+    document.querySelector("#col_2").classList.toggle("hidden")
+    document.querySelector("#col_3").classList.toggle("times2")
+
+}
