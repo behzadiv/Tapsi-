a22626aa16dc6a4520be4427c35adbad79759818
Behzad <saeedibehzad@gmail.com>
2021-12-19 09:17:09 +0330

    add banner and services section

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const showMenu=()=>{
-    document.querySelector(".nav__lists").classList.toggle("show__menu")  
-    document.querySelector("#col_1").classList.toggle("times1")
-    document.querySelector("#col_2").classList.toggle("hidden")
-    document.querySelector("#col_3").classList.toggle("times2")
+const showMenu = () => {
+  document.querySelector(".nav__lists").classList.toggle("show__menu");
+  document.querySelector("#col_1").classList.toggle("times1");
+  document.querySelector("#col_2").classList.toggle("hidden");
+  document.querySelector("#col_3").classList.toggle("times2");
+};
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab--content");
+//console.log(tabs[0].dataset.tabTarget);
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    const targetTabContent = document.querySelector(
+      `#${tab.dataset.tabTarget}`
+    );
+    //remove active class
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
 
-}
+    // add active class
+    tab.classList.add("active")
+    targetTabContent.classList.add("active")
+  });
+});
